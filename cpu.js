00a0eb5aa4be98abcb89fa5b14545747c8bdb52f
Earle Castledine <earlec@21csw.com>
2025-10-31 15:35:15 +0800

    mess

--- cpu.js
+++ cpu.js
@@ -15,7 +15,7 @@ const mk_regs = () => ({
     ir: 0, // internal instruction register
 });
 
-const pinout = {
+export const pinout = {
     A0: 0,
     A1: 1,
     A2: 2,
@@ -32,14 +32,21 @@ const pinout = {
     A13: 13,
     A14: 14,
     A15: 15,
+
+    D0: 16,
+    D1: 17,
+    D2: 18,
+    D3: 19,
+    D4: 20,
+    D5: 21,
+    D6: 22,
+    D7: 23,
+
+    RW: 24,
     SYNC: 25,
 };
-const mk_pins = () => {
-    // TODO: just use a number... everyone would be happier
-    const pins = malloc(40);
-    pins[pinout.SYNC] = true;
-    return pins;
-};
+// TODO: just use a number... everyone would be happier
+const mk_pins = () => malloc(40);
 
 const mk_flags = () => ({
     carry: false,
@@ -60,10 +67,26 @@ export const step = (compy) => {
 
 export const get_data = (pins) => {
     // return what's on the data bus.
-    return 0xa9;
+    return (
+        (pins[pinout.D0] << 0) +
+        (pins[pinout.D1] << 1) +
+        (pins[pinout.D2] << 2) +
+        (pins[pinout.D3] << 3) +
+        (pins[pinout.D4] << 4) +
+        (pins[pinout.D5] << 5) +
+        (pins[pinout.D6] << 6) +
+        (pins[pinout.D7] << 7)
+    );
 };
-const put_data = (pins, value) => {
-    // write value to bus.
+export const put_data = (pins, value) => {
+    pins[pinout.D0] = !!(value & (1 << 0));
+    pins[pinout.D1] = !!(value & (1 << 1));
+    pins[pinout.D2] = !!(value & (1 << 2));
+    pins[pinout.D3] = !!(value & (1 << 3));
+    pins[pinout.D4] = !!(value & (1 << 4));
+    pins[pinout.D5] = !!(value & (1 << 5));
+    pins[pinout.D6] = !!(value & (1 << 6));
+    pins[pinout.D7] = !!(value & (1 << 7));
 };
 
 export const get_addr = (pins) =>
@@ -84,56 +107,73 @@ export const get_addr = (pins) =>
     (pins[pinout.A14] << 14) +
     (pins[pinout.A15] << 15);
 
-const get_address_bus = (pins) => {
-    const addr = get_addr(pins);
-    console.log("get addr", addr);
-    return addr;
-};
-
-const put_address_bus = (pins, value) => {
-    pins[pinout.A0] = value & (1 < 0);
-    pins[pinout.A1] = value & (1 < 1);
-    pins[pinout.A2] = value & (1 < 2);
-    pins[pinout.A3] = value & (1 < 3);
-    pins[pinout.A4] = value & (1 < 4);
-    pins[pinout.A5] = value & (1 < 5);
-    pins[pinout.A6] = value & (1 < 6);
-    pins[pinout.A7] = value & (1 < 7);
-    pins[pinout.A8] = value & (1 < 8);
-    pins[pinout.A9] = value & (1 < 9);
-    pins[pinout.A10] = value & (1 < 10);
-    pins[pinout.A11] = value & (1 < 11);
-    pins[pinout.A12] = value & (1 < 12);
-    pins[pinout.A13] = value & (1 < 13);
-    pins[pinout.A14] = value & (1 < 14);
-    pins[pinout.A15] = value & (1 < 15);
-    console.log("put addr", value);
+const put_addr = (pins, value) => {
+    pins[pinout.A0] = value & (1 << 0);
+    pins[pinout.A1] = value & (1 << 1);
+    pins[pinout.A2] = value & (1 << 2);
+    pins[pinout.A3] = value & (1 << 3);
+    pins[pinout.A4] = value & (1 << 4);
+    pins[pinout.A5] = value & (1 << 5);
+    pins[pinout.A6] = value & (1 << 6);
+    pins[pinout.A7] = value & (1 << 7);
+    pins[pinout.A8] = value & (1 << 8);
+    pins[pinout.A9] = value & (1 << 9);
+    pins[pinout.A10] = value & (1 << 10);
+    pins[pinout.A11] = value & (1 << 11);
+    pins[pinout.A12] = value & (1 << 12);
+    pins[pinout.A13] = value & (1 << 13);
+    pins[pinout.A14] = value & (1 << 14);
+    pins[pinout.A15] = value & (1 << 15);
     return value;
 };
 
 const fetch = (cpu) => {
-    put_data(cpu.pins, cpu.regs.pc);
-    cpu.pins[pinout.SYNC] = false;
+    put_addr(cpu.pins, cpu.regs.pc);
+    cpu.pins[pinout.SYNC] = true;
+    console.log("put", cpu.regs.pc);
+};
+
+export const init_cpu = (cpu) => {
+    fetch(cpu);
+    cpu.pins[pinout.RW] = true;
+    cpu.pins[pinout.SYNC] = true;
+    return cpu.pins;
 };
 
 // Tick one clock cycle
 // https://floooh.github.io/2019/12/13/cycle-stepped-6502.html
 export const tick = (cpu) => {
     const { pins, regs } = cpu;
+    console.log("tick", regs.pc, regs.ir & 3);
     if (pins[pinout.SYNC]) {
         regs.ir = get_data(pins) << 3;
+        console.log("new instruction:", regs.ir);
         pins[pinout.SYNC] = false;
+        regs.pc++;
     }
+
+    pins[pinout.RW] = true;
+
     switch (regs.ir++) {
+        // LDX #
+        case (0xa2 << 3) | 0:
+            put_addr(pins, regs.pc++);
+            break;
+        case (0xa2 << 3) | 1:
+            regs.x = get_data(pins);
+            fetch(cpu);
+            break;
+
+        // LDA #
         case (0xa9 << 3) | 0:
-            put_address_bus(pins, regs.pc++);
-            console.log("tick 1 0xa9", regs.pc);
+            put_addr(pins, regs.pc++);
             break;
         case (0xa9 << 3) | 1:
-            console.log("tick 2 0xa9");
+            // c->A=_GD();_NZ(c->A);_FETCH();break ;
+            regs.a = get_data(pins);
+            // TODO: set nz
             fetch(cpu);
             break;
     }
-
     return pins;
 };
--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 import { malloc } from "./utils.js";
-import { mk_cpu, step, tick, get_addr, get_data } from "./cpu.js";
+import {
+    mk_cpu,
+    init_cpu,
+    step,
+    tick,
+    get_addr,
+    get_data,
+    put_data,
+    pinout,
+} from "./cpu.js";
 
 const cpu = mk_cpu();
 const mem = malloc(1 << 16);
 
 mem[0] = 0xa9;
-mem[1] = 0x01;
+mem[1] = 0x03;
+mem[2] = 0xa2;
+mem[3] = 0x05;
 
-console.log(cpu.regs.pc, mem);
-const pins = tick(cpu);
-const addr = get_addr(pins);
-mem[addr] = get_data(pins);
-console.log("oh hai", addr, mem[addr]);
+let pins = init_cpu(cpu);
+put_data(pins, mem[get_addr(pins)]);
+
+for (let i = 0; i < 4; i++) {
+    pins = tick(cpu);
+    put_data(pins, mem[get_addr(pins)]);
+    console.log("PC:", cpu.regs.pc);
+    const addr = get_addr(pins);
+    if (pins[pinout.RW]) {
+        console.log("PUTING ON DDT", addr, mem[addr]);
+        put_data(pins, mem[addr]);
+    } else {
+        mem[addr] = get_data(pins);
+    }
+}
+
+console.log("A:", cpu.regs.a, "X:", cpu.regs.x);
